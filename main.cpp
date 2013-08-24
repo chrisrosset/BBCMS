519d9cbd9f2ca1cba9a3a4a6abafb220eaee6116
Christopher Rosset <chris@rosset.org.uk>
2013-08-24 15:02:29 +0100

    wrote the view functions for the output messages

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <istream>
 #include <boost/shared_ptr.hpp>
 
 #include "enums.h"
@@ -6,114 +7,148 @@
 #include "parser.h"
 #include "orderstore.h"
 
+// functions for providing output Views
+
+std::string buildAggressResponse(const std::vector<Cmd::Post>& result)
+{
+    std::stringstream ss;
+    for(std::vector<Cmd::Post>::const_iterator it = result.begin();
+            it != result.end(); ++it) {
+        ss << (it->side == BUY ? "BOUGHT" : "SOLD") << " "
+           << it->amount << " "
+           << commodityToString(it->commodity) << " @ "
+           << it->price << " FROM "
+           << dealerIdToString(it->dealerId) << std::endl;
+    }
+
+    return ss.str();
+}
+
+std::string buildCheckResponse(const Cmd::Check& c, const Cmd::Post& p)
+{
+    std::stringstream ss;
+    ss << c.orderId << " ";
+
+    if(p.amount > 0)
+        ss << p.toString();
+    else
+        ss << "HAS BEEN FILLED";
+
+    ss << std::endl;
+    return ss.str();
+}
+
+std::string buildListResponse(std::vector<std::pair<OrderId, Cmd::Post> > result)
+{
+    std::stringstream ss;
+    for(unsigned int i = 0; i < result.size(); ++i) {
+        ss << result[i].first << " "
+           << result[i].second.toString()
+           << std::endl;
+    }
+    return ss.str();
+}
+
+std::string buildPostResponse(OrderId id, const Cmd::Post& c)
+{
+    std::stringstream ss;
+    ss << id << " " << c.toString() << " HAS BEEN POSTED" << std::endl;
+    return ss.str();
+}
+
+std::string buildRevokeResponse(OrderId orderId)
+{
+    std::stringstream ss;
+    ss << orderId << " HAS BEEN REVOKED" << std::endl;
+    return ss.str();
+}
+
+////
+
 void despatchCommand(OrderStore& store, Cmd::Command* ptr)
 {
-    if(Cmd::Check * c = dynamic_cast<Cmd::Check*>(ptr)) {
-        std::cout << "CHECK COMMAND"<< std::endl;
+    if(Cmd::Aggress * c = dynamic_cast<Cmd::Aggress*>(ptr)) {
+        std::vector<Cmd::Post> result;
+        Error e = store.aggress(*c, result);
+
+        if(e == NO_ERROR) {
+            std::cout << buildAggressResponse(result);
+        } else {
+            std::cout << errorToString(e) << std::endl;
+        }
+    } else if(Cmd::Check * c = dynamic_cast<Cmd::Check*>(ptr)) {
+        Cmd::Post p;
+        Error e = store.check(*c, p);
+
+        if(e == NO_ERROR) {
+            std::cout << buildCheckResponse(*c, p);
+        } else {
+            std::cout << errorToString(e) << std::endl;
+        }
     } else if(Cmd::List * c = dynamic_cast<Cmd::List*>(ptr)) {
 
         std::vector<std::pair<OrderId, Cmd::Post> > result;
-
         Error e = store.list(*c, result);
+
         if (e == NO_ERROR) {
-            for(int i = 0; i < result.size(); ++i) {
-                std::cout << result[i].first << " "
-                          << result[i].second.toString() 
-                          << std::endl;
-            }
+            std::cout << buildListResponse(result);
         } else {
-            std::cout << "Error in despatchCommand for Cmd::List" << std::endl;
+            std::cout << errorToString(e) << std::endl;
         }
 
     } else if(Cmd::Post * c = dynamic_cast<Cmd::Post*>(ptr)) {
-
         OrderId id = -1;
         Error e = store.post(*c, id);
         if(e == NO_ERROR) {
-            std::cout << id << " "
-                      << c->toString() 
-                      << " HAS BEEN POSTED"
-                      << std::endl;
+            std::cout << buildPostResponse(id, *c);
         } else {
-            std::cout << "THERE BE AN ERROR IN THIS BIATCH" << std::endl;
+            std::cout << errorToString(e) << std::endl;
         }
 
     } else if(Cmd::Revoke * c = dynamic_cast<Cmd::Revoke*>(ptr)) {
-        
-        switch(store.revoke(*c)) {
-            case UNKNOWN_ORDER:
-                std::cout << "UNKNOWN_ORDER";
-                break;
-            case UNAUTHORIZED:
-                std::cout << "UNAUTHORIZED";
-                break;
-            case NO_ERROR:
-                std::cout << c->orderId << " HAS BEEN REVOKED" << std::endl;
-                break;
+        Error e = store.revoke(*c);
+
+        if(e == NO_ERROR) {
+            std::cout << buildRevokeResponse(c->orderId);
+        } else {
+            std::cout << errorToString(e) << std::endl;
         }
-        
-    } else {
-        std::cout << "UNIMPLEMENTED COMMAND"<< std::endl;
     }
 }
+
 
 int main(int argc, char* argv[])
 {
     OrderStore store;
-    std::string input;
+    std::string temp;
 
-    while(1) {
-        std::getline(std::cin, input);
-        if(input.length() == 0)
+    while(true) {
+        char cstr[257];
+        std::cin.get(cstr, 257);
+
+        std::string input = cstr;
+        std::cout << input << std::endl;
+        if(input.length() > 255) {
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cerr << "INVALID MESSAGE" << std::endl;
+        } else if(input.length() == 0) {
             return 0;
+        }
 
+        std::getline(std::cin, temp);
 
         Cmd::Command* ptr = 0;
 
         // parse and perform error detection
-        switch( parseCommand(input, &ptr) ) {
-            case INVALID_MESSAGE:
-                std::cerr << "Invalid message." << std::endl;
-                break;
-            case UNKNOWN_DEALER:
-                std::cerr << "Unknown dealer." << std::endl;
-                break;
-            case NO_ERROR:
-                despatchCommand(store, ptr);
+        Error e = parseCommand(input, &ptr);
+        if(e == NO_ERROR) {
+            despatchCommand(store, ptr);
+        } else {
+            std::cout << errorToString(e) << std::endl;
         }
 
         if(ptr != 0)
             delete ptr;
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
