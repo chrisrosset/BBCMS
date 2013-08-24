ed680cba39af5b623b9a5bcb9169e31564def9ed
Christopher Rosset <chris@rosset.org.uk>
2013-08-24 19:08:40 +0100

    wrote the tcp code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <istream>
 #include <boost/shared_ptr.hpp>
+#include <boost/asio.hpp>
 
 #include "enums.h"
 #include "command.h"
@@ -30,7 +31,7 @@ std::string buildCheckResponse(const Cmd::Check& c, const Cmd::Post& p)
     ss << c.orderId << " ";
 
     if(p.amount > 0) {
-        ss << p.toString(); 
+        ss << p.toString();
    } else {
         ss << "HAS BEEN FILLED";
    }
@@ -66,27 +67,28 @@ std::string buildRevokeResponse(OrderId orderId)
 
 ////
 
-// Commodity Market System Controller - coordinate parsed input 
+// Commodity Market System Controller - coordinate parsed input
 // and provide output in the form of system messages
-void despatchCommand(OrderStore& store, Cmd::Command* ptr)
+std::string despatchCommand(OrderStore& store, Cmd::Command* ptr)
 {
+    std::stringstream ss;
     if(Cmd::Aggress * c = dynamic_cast<Cmd::Aggress*>(ptr)) {
         std::vector<Cmd::Post> result;
         Error e = store.aggress(*c, result);
 
         if(e == NO_ERROR) {
-            std::cout << buildAggressResponse(result);
+            ss << buildAggressResponse(result);
         } else {
-            std::cout << errorToString(e) << std::endl;
+            ss << errorToString(e) << std::endl;
         }
     } else if(Cmd::Check * c = dynamic_cast<Cmd::Check*>(ptr)) {
         Cmd::Post p;
         Error e = store.check(*c, p);
 
         if(e == NO_ERROR) {
-            std::cout << buildCheckResponse(*c, p);
+            ss << buildCheckResponse(*c, p);
         } else {
-            std::cout << errorToString(e) << std::endl;
+            ss << errorToString(e) << std::endl;
         }
     } else if(Cmd::List * c = dynamic_cast<Cmd::List*>(ptr)) {
 
@@ -94,31 +96,81 @@ void despatchCommand(OrderStore& store, Cmd::Command* ptr)
         Error e = store.list(*c, result);
 
         if (e == NO_ERROR) {
-            std::cout << buildListResponse(result);
+            ss << buildListResponse(result);
         } else {
-            std::cout << errorToString(e) << std::endl;
+            ss << errorToString(e) << std::endl;
         }
     } else if(Cmd::Post * c = dynamic_cast<Cmd::Post*>(ptr)) {
         OrderId id = -1;
         Error e = store.post(*c, id);
-        
+
         if(e == NO_ERROR) {
-            std::cout << buildPostResponse(id, *c);
+            ss << buildPostResponse(id, *c);
         } else {
-            std::cout << errorToString(e) << std::endl;
+            ss << errorToString(e) << std::endl;
         }
     } else if(Cmd::Revoke * c = dynamic_cast<Cmd::Revoke*>(ptr)) {
         Error e = store.revoke(*c);
 
         if(e == NO_ERROR) {
-            std::cout << buildRevokeResponse(c->orderId);
+            ss << buildRevokeResponse(c->orderId);
         } else {
-            std::cout << errorToString(e) << std::endl;
+            ss << errorToString(e) << std::endl;
         }
     }
+
+    return ss.str();
 }
 
-int main(int argc, char* argv[])
+
+int controlTcp()
+{
+    using boost::asio::ip::tcp;
+
+    boost::asio::io_service io_service;
+    tcp::endpoint endpoint(tcp::v4(), 8080);
+    tcp::acceptor acceptor(io_service, endpoint);
+
+    tcp::iostream stream;
+    boost::system::error_code ec;
+    acceptor.accept(*stream.rdbuf(), ec);
+
+    OrderStore store;
+    std::string temp;
+
+    while(true) {
+        char cstr[257];
+        stream.get(cstr, 257);
+
+        std::string input = cstr;
+        stream << input << std::endl;
+        if(input.length() > 255) {
+            stream.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            stream << "INVALID_MESSAGE" << std::endl;
+        } else if(input.length() == 0) {
+            return 0;
+        }
+
+        std::getline(stream, temp);
+
+        // parse and perform error detection
+        Cmd::Command* ptr = 0;
+        Error e = parseCommand(input, &ptr);
+        if(e == NO_ERROR) {
+            std::string a = despatchCommand(store, ptr);
+            stream << a;
+            std::cout << a;
+        } else {
+            stream << errorToString(e) << std::endl;
+        }
+
+        if(ptr != 0)
+            delete ptr;
+    }
+
+}
+
+int controlStdio()
 {
     OrderStore store;
     std::string temp;
@@ -131,25 +183,39 @@ int main(int argc, char* argv[])
         std::cout << input << std::endl;
         if(input.length() > 255) {
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cerr << "INVALID_MESSAGE" << std::endl;
+            std::cout << "INVALID_MESSAGE" << std::endl;
         } else if(input.length() == 0) {
             return 0;
         }
 
         std::getline(std::cin, temp);
 
-        Cmd::Command* ptr = 0;
-
         // parse and perform error detection
+        Cmd::Command* ptr = 0;
         Error e = parseCommand(input, &ptr);
         if(e == NO_ERROR) {
-            despatchCommand(store, ptr);
+            std::cout << despatchCommand(store, ptr);
         } else {
             std::cout << errorToString(e) << std::endl;
         }
 
         if(ptr != 0)
             delete ptr;
+    }
+}
+
+
+int main(int argc, char* argv[])
+{
+    if (argc == 2 && 0 == strcmp(argv[1], "base")) {
+        return controlStdio();
+    } else if (argc == 3 && 0 == strcmp(argv[1], "ext1")) {
+        return controlTcp();
+    } else if (argc == 3 && 0 == strcmp(argv[1], "ext2")) {
+        return 1;
+    } else {
+        std::cout << "Help message. Very helpful." << std::endl;
+        return 0;
     }
 }
 
